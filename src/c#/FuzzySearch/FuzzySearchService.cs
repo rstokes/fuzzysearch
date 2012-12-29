e1154d5442e283df6c897646e2be3de9be11a592
Rob Stokes <rob.lee.stokes@gmail.com>
2012-12-29 11:52:44 -0600

    Adding intial c# project

---
+++ src/c#/FuzzySearch/FuzzySearchService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace FuzzySearch
+{
+    public class FuzzySearchService
+    {
+        private Dictionary<string, string> index;
+
+        private IEnumerable<string> ParseValue(string value)
+        {
+            return value.ToLower().Split(' ');
+        }
+
+        private void Init(IEnumerable<string> valuesToIndex)
+        {
+            var indexToBuild = new Dictionary<string, string>();
+
+            foreach (var item in valuesToIndex)
+            {
+               
+            }
+        }
+
+        public FuzzySearchService(IEnumerable<string> valuesToIndex)
+        {
+            Init(valuesToIndex);
+        }
+    }
+}
---
+++ src/c#/Runner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FuzzySearch;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var searchObj = new FuzzySearchService(new List<string> {"values", "to try", "and index"});
+            Console.ReadKey();
+        }
+    }
+}
